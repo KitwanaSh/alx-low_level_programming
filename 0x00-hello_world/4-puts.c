6c4a6a764213ce3b617017b01c585590389a2aab
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-06-30 12:40:29 -0700

    Readd the task 4 of the HW project

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Create the output with puts
+ *
+ * Utilize the function puts
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle\n");
+
+  return 0;
+}
