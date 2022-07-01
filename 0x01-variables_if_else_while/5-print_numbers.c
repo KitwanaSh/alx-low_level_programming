f152e4512a4df34f763da4524693fc4db78bd207
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-01 07:29:29 -0700

    Add the task 5 of the loops project

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * Printing 10 digint numbers from 0
+ * Has to return  - SUCCESS ---
+ */
+int main(void)
+{
+  int num = 0;
+
+  while (num < 10)
+    {
+      printf("%d\n", num);
+      num++;
+    }
+
+  return (0);
+}
