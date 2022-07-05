b934ce988d73a6227a327a9aadf910761bf344be
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-05 11:32:57 -0700

    Add the task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check your code
+ *
+ * Return: nothing
+ */
+void print_alphabet(void)
+{
+	int c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -2,6 +2,7 @@
 
 /**
  * _putchar - writes the character c to stdout
+ * print_aphabet - write all the alphabet (a - z)
  * @c: The character to print
  *
  * Return: On success 1.
