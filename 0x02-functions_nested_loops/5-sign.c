0d9eae6b84aa5a6996f25a47b6fee7219884660f
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-06 12:29:02 -0700

    Update the main.h with prototype 6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * print_sign - Shows 1 and print + if the number is greater than 0;
- * shows 0 and prints 0 is the number is zero;
+ * print_sign - Returns 1 and print + if the number is greater than 0;
+ * returns 0 and prints 0 is the number is zero;
  * and lastly show -1 if the number is less than zero
+ * @n: represents the numerical numbers in characters
  *
- * @n : represents the numerical numbers in characters
- *
- * Description : print_sign shows 1 if number is
+ * Description: print_sign shows 1 if number is
  * greater than 0, and -1 if it is negative and other if 0
  */
 int print_sign(int n)
@@ -27,4 +26,5 @@ int print_sign(int n)
 		_putchar('-');
 		return (-1);
 	}
+	return (0);
 }
