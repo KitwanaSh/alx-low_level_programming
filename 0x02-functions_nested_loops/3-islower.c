98fee58e758de4d56175c1f1378ab7f4604cc889
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-06 07:11:20 -0700

    Update the task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- *_islower - Shows 1 if the character is in lowercase and 0 if the other
+ *_islower - Shows 1 if the character is in
+ *lowercase and 0 if the other
+ *
  *@c : Representing all the characters in ASCII code
  *
  *Return : 1 for lowecrcase and 0 for other
