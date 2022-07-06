f0b0430b405dc07b3c9aed05204aba27a6869f79
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-06 00:51:47 -0700

    Refactor: Update the betty coding style

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,9 @@ void print_alphabet_x10(void)
 
 	int alpha = 0, c;
 
-	while(alpha < 10)
+	while (alpha < 10)
 	{
-		for(c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
