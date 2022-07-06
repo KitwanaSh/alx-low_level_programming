205d90653c7d00e2daf0c6c8282424eb527d8519
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-06 06:07:58 -0700

    Update the task 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,11 @@
 /**
  *_islower - Entry checks if the character is lowercase
  *@c : Representing all the characters
- *Descritption: Recall the use of c and how the function _islower relates to it.
+ *Descritption: Recall the use of c
  *
  *Return : Always return @result.
  */
-int _islower(int c) /* The character should run here*/
+int _islower(int c)
 {
 	int result;
 
