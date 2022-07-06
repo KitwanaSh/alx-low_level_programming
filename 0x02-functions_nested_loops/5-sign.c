285c8480cb00e18116a71bc162889b148bb6b269
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-06 07:53:16 -0700

    Add task 5 of the req

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,23 +7,24 @@
  *
  * @n : represents the numerical numbers in characters
  *
- * Return : 1 if number is greater than 0, and -1 if it is negative and other if 0
+ * Description : print_sign shows 1 if number is
+ * greater than 0, and -1 if it is negative and other if 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
-		return (0);
 		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		return (-1);
 		_putchar('-');
+		return (-1);
 	}
 }
