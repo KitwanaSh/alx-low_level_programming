1ad25061932dc083299b2030394aee41b6af6be2
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-07 08:03:10 -0700

    Add the task 2 from the more nested project

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - get the multiplication of two numbers
+ * @a: this is the first number to be input
+ * @b:  this is the second number to be input
+ *
+ * Return: only the multiplication of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
