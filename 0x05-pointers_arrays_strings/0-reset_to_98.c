32edf392ba3e608038d87b91ccdcae12930ba9ca
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-11 03:14:50 -0700

    Add the task 0 to the pointer_array project

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  resets the value of n to 98
+ * @*n: The integer n with asteric represent the pointer of a number
+ *
+ * Return: get the value of initial n and bring back 98
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
