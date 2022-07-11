85f057dba3325c096d7588ff6e8345aecdaf0dea
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-11 14:10:19 -0700

    Add the task task pf the project array_pointer

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "main.h"
+
+/**
+*main - Randomly generate pass
+*
+*Return: nothing
+*/
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+
+	sum = 0;
+
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+	printf("%c", 2772 - sum);
+	return (0);
+}
