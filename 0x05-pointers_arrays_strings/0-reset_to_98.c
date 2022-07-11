36443cf6b3b365e538bdd148e47997410dfa395f
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-11 03:17:20 -0700

    Refactor the betty style for the parameter n

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 -  resets the value of n to 98
- * @*n: The integer n with asteric represent the pointer of a number
+ * @n: The integer n with asteric represent the pointer of a number
  *
  * Return: get the value of initial n and bring back 98
  */
