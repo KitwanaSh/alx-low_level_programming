47c6ac9c3e2ca83db316bee2926e8b3c62177320
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-14 00:13:20 -0700

    Refractor the 3-strcmp.c file

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,8 @@
  * @s1: first string
  * @s2: second string
  *
- * Return: greater than 0 if s1 > s2, 0 if s1 and s2 are the same, and less then 0 for the other
+ * Return: greater than 0 if s1 > s2, 0 if s1 and s2 are
+ * the same, and less then 0 for the other
  */
 int _strcmp(char *s1, char *s2)
 {
