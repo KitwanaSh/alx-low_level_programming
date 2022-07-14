ea28501dd9b997207b5eefab946d21368762f7a8
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-14 00:09:55 -0700

    Refractor task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 
 /**
  * _strcmp - compares the first string to the second
@@ -10,20 +9,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int flag = 0;
-
-	while (*s1 != '\0' && *s2 != '\0')
+	while ((*s1 != '\0' && *s2 != '\0') && *s1 == *s2)
 	{
-		if (*s1 != *s2)
-		{
-			flag = 1;
-		}
 		s1++;
 		s2++;
 	}
-	if (flag == 0)
+	if (*s1 == *s2)
+	{
 		return (0);
+	}
 	else
-		return (1);
-	return (0);
+	{
+		return (*s1 - *s2);
+	}
 }
