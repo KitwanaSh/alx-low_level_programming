710002953c1ad6be13dcea9b8925a3201fa509ab
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-14 00:45:50 -0700

    Refractor task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,27 +12,27 @@ char *cap_string(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] == 0)
+		if (str[0] >= 'a' && str[0] <= 'z')
 		{
-			if (str[i] >= 'a' && str[i] <= 'z')
-			{
-				str[i] = str[i] - 32;
-				continue;
-			}
+			str[0] = str[0] - 32;
 		}
-		if (str[i] == ' ' || str[i] == '\n' || str[i] == '.')
+		if (str[i] == ' ' ||
+		str[i] == '.' ||
+		str[i] == ',' ||
+		str[i] == ';' ||
+		str[i] == '!' ||
+		str[i] == '?' ||
+		str[i] == '(' ||
+		str[i] == ')' ||
+		str[i] == '{' ||
+		str[i] == '}' ||
+		str[i] == '\t' ||
+		str[i] == '\n')
 		{
-			i++;
-			if (str[i] >= 'a' && str[i] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
-				str[i] = str[i] - 32;
-				continue;
+				str[i + 1] = str[i + 1] - 32;
 			}
-		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
 		}
 	}
 	return (str);
