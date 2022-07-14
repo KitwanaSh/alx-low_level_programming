27254aa9ece47911dadbf8de42eec95279dacc45
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-13 23:56:23 -0700

    Add task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalize every word
+ * @str: the string to converst in word
+ *
+ * Return:  the string turned into a capitalized
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 0)
+		{
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		if (str[i] == ' ' || str[i] == '\n' || str[i] == '.')
+		{
+			i++;
+			if (str[i] >= 'a' && str[i] <= 'z')
+			{
+				str[i] = str[i] - 32;
+				continue;
+			}
+		}
+		else
+		{
+			if (str[i] >= 'A' && str[i] <= 'Z')
+				str[i] = str[i] + 32;
+		}
+	}
+	return (str);
+}
+
