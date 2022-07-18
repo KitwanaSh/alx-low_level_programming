dbc8cc6473748eebcc4e7688508f7d8e1f7e3bf6
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-18 12:12:45 -0700

    Add the crackme task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - set a value of a pointer to a char
+ * @s: th double pointer of type char
+ * @to:  apointer of type char
+ *
+ * Return: Nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
