fd772429fe06fb433529e24b208937fb7966e17d
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-20 01:43:39 -0700

    Ref: Update task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _puts_recursion - print a strings
@@ -9,12 +8,13 @@
  */
 void _puts_recursion(char *s)
 {
-	if ((*s >= 'a' && *s <= 'z')
-	|| (*s >= 'A' && *s <= 'Z')
-	|| (*s == ' '))
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+	else
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
-		_putchar('\n');
 	}
 }
