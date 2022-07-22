cc0ce1e25a4e9007aa62836bd06bb3df7b1c4e74
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-22 10:14:23 -0700

    Feat: add elements of task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main -  the main function
+ * @argc: Counts arguments passed variable
+ * @argv: Argument vector string variable
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
