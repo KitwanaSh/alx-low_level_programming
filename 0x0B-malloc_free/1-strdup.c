0e677333e559cb55c7718fd247a8d8a50d59b2da
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-25 06:29:29 -0700

    Feat: Add element of the task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - copy a string and return a pointer
+ * @str: pointer of a type string
+ *
+ * Return: a copy of a pointer in string
+ */
+char *_strdup(char *str)
+{
+	unsigned int len = strlen(str) + 1;
+	char *result = (char *) malloc(len);
+
+	if (result == NULL)
+		return (NULL);
+	return ((char *) memcpy(result, str, len));
+}
