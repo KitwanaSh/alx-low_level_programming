6a3cc855994ad848096e82b043c35dfa283a0211
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-26 05:02:43 -0700

    Feat: add elements of task 4

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - free the 2D array
+ * @grid: pointer of pointer int
+ * @height: variable int
+ *
+ * Return: nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	for (height = height - 1; height <= 0; height--)
+		free(grid[height]);
+	free(grid);
+}
