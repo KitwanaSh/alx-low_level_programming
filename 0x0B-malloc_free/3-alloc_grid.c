b58806bcf65476878177f9c40c809d56032db2c5
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-26 04:44:45 -0700

    Feat: Add elements of task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - returns a pointer to a 2D arrays
+ * @width: first dim
+ * @hight: second dim
+ *
+ * Return: NULL on failure or if weight or height
+ * is less than or equal to 0.
+ */
+int **alloc_grid(int width, int height)
+{
+	int m, n, o, **ch;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+	ch = malloc(height * sizeof(*ch));
+	if (ch == NULL)
+		return (ch);
+	for (m = 0; m < height; m++)
+	{
+		ch[m] = malloc(width * sizeof(int));
+		if (ch[m] == NULL)
+		{
+			for (o = 0; o < m; o++)
+				free(ch[o]);
+			free(ch);
+			return (NULL);
+		}
+		for (n = 0; n < width; n++)
+			ch[m][n] =0;
+	}
+	return (ch);
+}
