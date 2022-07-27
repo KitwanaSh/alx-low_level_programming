615d4d843731b584d862f7d82fa8cad9ea28c59a
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-27 01:11:56 -0700

    Fix: modify tak 4

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,7 +10,7 @@
  */
 void free_grid(int **grid, int height)
 {
-	for (height -= 1; height <= 0; height--)
+	for (height = height - 1; height >= 0; height--)
 		free(grid[height]);
 	free(grid);
 }
