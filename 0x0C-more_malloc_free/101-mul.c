0556612cd3cf91b1bf7aa2b4c70c6b4dc03201b1
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-29 02:06:29 -0700

    Feat: Add elements of the task 5

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main function
+ * @argc: The count of arguments
+ * @argv: The array of argument in char
+ *
+ * Return: The multiplication of two numbers
+ */
+int main(int argc, char *argv[])
+{
+	int n = argc;
+	int mul, *num1, *num2;
+
+	num1 = malloc(sizeof(int) * 10);
+	num2 = malloc(sizeof(int) * 10);
+
+	if (n != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (*num1 >= 10 || *num2 >= 10)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	*num1 = atoi(argv[1]);
+	*num2 = atoi(argv[2]);
+	mul = *num1 * *num2;
+
+	printf("%d\n", mul);
+	free(num1);
+	free(num2);
+	return (0);
+}
