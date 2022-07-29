af3602824397f7fe1276ee07bea5ce73250000ab
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-07-29 07:27:03 -0700

    Fix: Update task 4

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -34,6 +34,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		old_size = new_size;
 	while (old_size--)
 		p[old_size] = ((char *)ptr)[old_size];
-	return (ptr);
 	free(ptr);
+	return (ptr);
 }
