06023f0a70e93e49b6778e19a5fcc718a5e960d3
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-01 10:00:20 -0700

    Feat: Update header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,4 +16,7 @@ struct dog
 /* struct dog my_dog(char *name, float age, char *owner) */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
