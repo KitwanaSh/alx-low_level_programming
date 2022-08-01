02fbd00143f41e0c6f546ad490724399901e8d56
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-01 03:53:58 -0700

    Feat: Add elements of thet task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struc dog - the defined structure
+ * @name: variable char in pointer
+ * @age: float variable
+ * @owner: pointer variable of type char
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+/* struct dog my_dog(char *name, float age, char *owner) */
