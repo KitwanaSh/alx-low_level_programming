ffe937c651172d650510629abe669b6efc7bce48
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-02 01:29:18 -0700

    Fix: update the new_dog file

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "dog.h"
 
+/**
+ * new_dog - Create a new dog function
+ * @name: dog name in varible pointer
+ * @age: dog age in float
+ * @owner: dog owner in pointer variable
+ *
+ * Return: NULL or values
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+	d = malloc(sizeof(dog_t));
+	if (d == NULL)
+		return (NULL);
+	d->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (d->owner == NULL)
+	{
+		free(name);
+		free(d);
+		return (NULL);
+	}
+	d->name = strcpy(d->name, name);
+	d->age = age;
+	d->owner = strcpy(d->owner, owner);
+
+	return (d);
+}
