b66346b607661abf517539e0762d6de05fd1ffa8
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-04 11:04:18 -0700

    feat: add elements of the task3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - print all kind of data type
+ * @format: list of arguments passed to the function
+ *
+ * Return: Every kind of data type
+ */
+void print_all(const char * const format, ...)
+{
+	va_list ev;
+	unsigned int i = 0;
+	char *str;
+
+	va_start(ev, format);
+
+	while (format && format[i])
+	{
+		if (i && (format[i] == 'c' || format[i] == 'i' ||
+			format[i] == 'f' || format[i] == 's'))
+			printf(", ");
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(ev, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ev, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ev, double));
+				break;
+			case 's':
+				str = va_arg(ev, char *);
+
+				if (str == NULL)
+					printf("(nil)");
+				printf("%s", str);
+				break;
+			default:
+				break;
+		}
+		i++;
+	}
+
+	printf("\n");
+
+	va_end(ev);
+}
