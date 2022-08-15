4b6814cc66c8b2019e4067ec9ab1f107ad13d7c9
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-15 08:30:10 -0700

    Feat: add element of task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t lists
+ * @head: pointer to pointer to head
+ *
+ * Return: Nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *p;
+	listint_t **temp = head;
+
+	if (temp != NULL)
+	{
+		while (*head != NULL)
+		{
+			p = *head;
+			free(p);
+			*head = (*head)->next;
+		}
+		*temp = NULL;
+	}
+}
