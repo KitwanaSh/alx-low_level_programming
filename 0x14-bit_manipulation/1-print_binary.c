44baa2cf78feb44d8606d709a350651ac48930a1
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-18 10:36:14 -0700

    Fix: debug

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,24 +8,7 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (!n)
-	{
-		_putchar('0');
-		return;
-	}
-	spot_print(n, 1);
-}
-
-/**
- * spot_print -print an in in binary
- * @n: the number to convert
- * @i: the times it will be calculated
- */
-
-void spot_print(unsigned long int n, unsigned long int i)
-{
-	if (i > n)
-		return;
-	spot_print(n, i << 1);
-	_putchar(n & i ? '1' : '0');
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
