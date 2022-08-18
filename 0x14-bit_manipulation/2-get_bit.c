2aeee3e5a3d5dfdecf20e81bd1e46c3dc636225d
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-18 06:12:55 -0700

    Feat: Add elements of task 2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * get_bit - give the value of a bit at a given index
+ * @n: the normal number
+ * @index: the index, starting from 0 of the bit to get
+ *
+ * Return: The value of a bit at index index, or
+ * -1 if an error happened
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned int num = 0, i = 1;
+
+	while (num++ < index)
+	{
+		i = i << 1;
+	}
+	if (i > n)
+		return (-1);
+	return (n & i ? 1 : 0);
+}
