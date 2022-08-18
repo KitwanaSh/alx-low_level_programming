01118776aa52d1fe32632f0993ffb40627938ece
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-18 05:47:25 -0700

    Feat: Add elements of the task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_binary - print a number in its binary version
+ * @n: the number to print
+ *
+ * Return: Nothing or no
+ */
+void print_binary(unsigned long int n)
+{
+	if (!n)
+	{
+		_putchar('0');
+		return;
+	}
+	else
+		spot_print(n, 1);
+}
+
+/**
+ * spot_print -print an in in binary
+ * @n: the number to convert
+ * @i: the times it will be calculated
+ */
+
+void spot_print(unsigned long int n, unsigned long int i)
+{
+	if (i > n)
+		return;
+	spot_print(n, i << 1);
+	_putchar(n & i ? '1' : '0');
+}
