fc2427117148ced2659b57c3077f256d5206d5a4
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-18 03:21:43 -0700

    Feat: Initial commit

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.c>
+
+/**
+ * _putchar - prints one character
+ * @c: the input char
+ *
+ * Return: Always 1 for success.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
