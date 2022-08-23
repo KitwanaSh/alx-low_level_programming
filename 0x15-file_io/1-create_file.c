bd0a7e5068542047ac10d757ecc343ba3140e086
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-22 23:28:01 -0700

    Fix: debut the function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,26 +10,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int cr;
-	char *buf;
+	int cr, wr, len = 0;
 
-	if (text_content == NULL)
-		cr = open(filename, O_CREAT, 2);
-	cr = open(filename, O_CREAT, 6);
-
-	buf = malloc(sizeof((char *) text_content));
-
-	if (buf == NULL)
+	if (text_content != NULL)
 	{
-		free(buf);
-		return (-1);
+		for (len = 0; text_content[len];)
+			len++;
 	}
-	free(buf);
-	close(cr);
+	cr = open(filename, O_CREAT | O_RDWR, O_TRUNC, 0600);
+	wr = write(cr, text_content, len);
 
 	if (filename == NULL)
 		return (-1);
-	if (cr == -1)
+	if (cr == -1 || wr == -1)
 		return (-1);
+
+	close(cr);
 	return (1);
 }
