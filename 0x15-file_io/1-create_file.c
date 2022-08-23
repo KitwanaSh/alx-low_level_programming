ea8ee812d9430262fc400bde8c811819668d1f0e
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-22 23:34:01 -0700

    Fix: Redebug function

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 		for (len = 0; text_content[len];)
 			len++;
 	}
-	cr = open(filename, O_CREAT | O_RDWR, O_TRUNC, 0600);
+	cr = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(cr, text_content, len);
 
 	if (filename == NULL)
