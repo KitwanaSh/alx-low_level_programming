883f72e26f0f5015c6f3db5b6efaf829b2115332
Kitwana S. Ezechiel <rodriguekitweze@gmail.com>
2022-08-24 02:49:21 -0700

    Fix: meet the betty expectation

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -59,13 +59,13 @@ int main(int argc, char *argv[])
 				"Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	buffer = create_buffer(argv[2]);
 	rd = read(file_from, buffer, 1024);
 
-	do {
+	if (rd > 0)
+	{
 		if (file_from == -1 || rd == -1)
 		{
 			dprintf(STDERR_FILENO,
@@ -73,7 +73,6 @@ int main(int argc, char *argv[])
 			free(buffer);
 			exit(98);
 		}
-
 		wr = write(file_to, buffer, rd);
 
 		if (file_to == -1 || wr == -1)
@@ -83,14 +82,11 @@ int main(int argc, char *argv[])
 			free(buffer);
 			exit(99);
 		}
-
 		file_from = open(argv[2], O_WRONLY, O_APPEND);
 		rd = read(file_from, buffer, 1024);
-	} while (rd > 0);
-
+	}
 	free(buffer);
 	close_buf(file_from);
 	close_buf(file_to);
-
 	return (0);
 }
