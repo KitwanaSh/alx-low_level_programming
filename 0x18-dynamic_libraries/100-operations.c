6620f926f35eb8b2dc591c8a8feebc4539d362f3
KitwanaSh <rodriguekitweze@gmail.com>
2022-10-04 11:09:12 +0200

    Feat: add the third task

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - adds two numbers
+ * @a: first number
+ * @b: the second number
+ * return: the sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a: first number
+ * @b: the second number
+ * return: the sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a: first number
+ * @b: the second number
+ * return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a: first number
+ * @b: the second number
+ * return: the div
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - finds the module two numbers
+ * @a: first number
+ * @b: the second number
+ * return: the module of a and b
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
