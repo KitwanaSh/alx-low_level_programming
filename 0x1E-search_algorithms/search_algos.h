96c528d8003b8f3a6343ce9141681f33ae4e134e
KitwanaSh <rodriguekitweze@gmail.com>
2023-01-28 13:12:38 +0200

    Refr: update the documentation

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,11 +7,13 @@
 
 /**
  * struct listint_s - singly linked list
- * @n: Integer number
+ *
+ * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
+ *
+ * Description: singly linked list node structure
  */
-
 typedef struct listint_s
 {
     int n;
@@ -20,13 +22,15 @@ typedef struct listint_s
 } listint_t;
 
 /**
- * struct skiplist_s - linked list with an express lane
- * @n: the integer number
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
  * @index: Index of the node in the list
  * @next: Pointer to the next node
  * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
  */
-
 typedef struct skiplist_s
 {
     int n;
